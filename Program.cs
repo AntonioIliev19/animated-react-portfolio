6edb93e93a9b10a0987ec833af90e631949cd0a5
Antonio Iliev <114621404+AntonioIliev19@users.noreply.github.com>
2024-04-27 13:50:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer("John", "Doe", false, true); // Bank client
+            Customer customer2 = new Customer("Jane", "Smith", true, true); // Bank client and pensioner
+            Customer customer3 = new Customer("Jim", "Beam", false, false); // Not a bank client
+
+            CreditAccount creditAccount = new CreditAccount("BankClient1", 1000);
+
+            creditAccount.GetCredit(500, customer1); // Should calculate with bank client interest rate
+            creditAccount.GetCredit(500, customer2); // Should calculate with pensioner interest rate
+            creditAccount.GetCredit(500, customer3); // Should calculate with default high interest rate
+        }
+
+    }
+}
---
+++ SavingsAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class SavingsAccount
+    {
+        protected string owner;
+        protected decimal balance;
+
+        public SavingsAccount(string name, decimal balance)
+        {
+            this.owner = name;
+            this.balance = balance;
+        }
+
+        public virtual void GetCredit(decimal creditAmount, Customer obj)
+        {
+            decimal totalCreditAmount;
+
+            totalCreditAmount = creditAmount + creditAmount * (decimal)0.08;
+            Console.WriteLine($"The Credit amount is ${totalCreditAmount} and the person is {obj.FirstName} ");
+        }
+    }
+}
